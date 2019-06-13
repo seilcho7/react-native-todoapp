78e837bfa20104094f6510271d55770c36418974
Seil Cho <seilcho7@hotmail.com>
2019-06-13 14:02:35 -0400

    adds bundleIdentifer

--- components/Header.js
+++ components/Header.js
@@ -11,13 +11,13 @@ export default function Header(props) {
 
 const styles = StyleSheet.create({
     header: {
-        backgroundColor: 'black',
+        // backgroundColor: 'black',
         height: 60,
         alignItems: 'center',
         justifyContent: 'center'
     },
     title: {
-        color: 'white',
+        color: 'black',
         fontSize: 28,
         fontWeight: '900',
         textTransform: 'uppercase'
